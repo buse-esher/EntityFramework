aa80c22ae6418c5b1ae3988ae5e745c67705e9dd
Buse Eşer <buseesher@gmail.com>
2023-10-09 12:22:22 +0300

    Yetgen Jump

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using EntitiyFrameworkAddDeleteExamples.Entities;
+using EntitiyFrameworkAddDeleteExamples.Persistence;
+
+Console.WriteLine("Entitiy Framework Core- InMemory");
+
+NoteMasterDbContext _context = new();
+
+_context.Notes.Add(new("Note1"));
+_context.Notes.Add(new("Note2"));
+_context.Notes.Add(new("Note3"));
+
+_context.SaveChanges();
+
+List<Note> notes=_context.Notes.ToList();
+
+Console.WriteLine();
